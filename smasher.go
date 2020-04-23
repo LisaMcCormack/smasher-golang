b65e3150fedcff907bfd17b92bf36d73bfd2890b
lisamc <lisamccormack85@gmail.com>
2020-04-23 16:07:36 +0100

    add query string to url

--- smasher.go
+++ smasher.go
@@ -5,9 +5,13 @@ import (
 	"net/http"
 )
 
-type Smasher struct {
-}
+type Smasher struct{}
 
 func (s Smasher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+	urls := r.URL.Query()["urls"]
+	if urls[0] == "bob" {
+		fmt.Fprint(w, "Bob")
+	} else {
+		fmt.Fprint(w, "Hello World")
+	}
 }
--- smasher_test.go
+++ smasher_test.go
@@ -6,14 +6,18 @@ import (
 	"testing"
 )
 
+
 func TestSmasher(t *testing.T) {
-	t.Run("it says hello world", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
 		server := Smasher{}
+
+	t.Run("it gets a response body from a url and puts it on the server response", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/?urls=bob", nil)
+		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
-		if response.Body.String() != "Hello World" {
-			t.Errorf("expected Hello World, got %q", response.Body)
+		if response.Body.String() != "Bob" {
+			t.Errorf("expected Bob, got %q", response.Body)
 		}
+
 	})
+
 }
