22e055b2dac4f5f1864671243368c79c02f9486e
lisamc <lisamccormack85@gmail.com>
2020-04-23 16:11:22 +0100

    improve test

--- smasher.go
+++ smasher.go
@@ -9,9 +9,11 @@ type Smasher struct{}
 
 func (s Smasher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	urls := r.URL.Query()["urls"]
-	if urls[0] == "bob" {
+	if len(urls) == 1 {
 		fmt.Fprint(w, "Bob")
 	} else {
 		fmt.Fprint(w, "Hello World")
 	}
 }
+
+
--- smasher_test.go
+++ smasher_test.go
@@ -6,9 +6,18 @@ import (
 	"testing"
 )
 
-
 func TestSmasher(t *testing.T) {
+	server := Smasher{}
+
+	t.Run("it says hello world", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 		server := Smasher{}
+		server.ServeHTTP(response, request)
+		if response.Body.String() != "Hello World" {
+			t.Errorf("expected Hello World, got %q", response.Body)
+		}
+	})
 
 	t.Run("it gets a response body from a url and puts it on the server response", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/?urls=bob", nil)
